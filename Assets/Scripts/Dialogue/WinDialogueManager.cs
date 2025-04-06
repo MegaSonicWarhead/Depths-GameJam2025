bc8d42964ca64152f8d1d49daaded88b8d3f7623
Palesa2323 <palesagaetsewe52@gmail.com>
2025-04-06 23:24:20 +0200

    WinDialogue

---
+++ Assets/Scripts/Dialogue/WinDialogueManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class WinDialogueManager : MonoBehaviour
+{
+    public DialogueData dialogueData;
+    public TextMeshProUGUI speakerText;
+    public TextMeshProUGUI dialogueText;
+    public Image portraitImage;
+    public GameObject dialoguePanel;
+    public GameObject nextButton;
+    public GameObject returnButton;
+
+    private int currentLine = 0;
+
+    void Start()
+    {
+        dialoguePanel.SetActive(true);
+        nextButton.SetActive(true);  // Show the Next button
+        returnButton.SetActive(false); // Hide the Return button initially
+        ShowLine();
+    }
+
+    public void ShowLine()
+    {
+        if (currentLine < dialogueData.lines.Length)
+        {
+            DialogueLine line = dialogueData.lines[currentLine];
+            speakerText.text = line.speaker;
+            dialogueText.text = line.text;
+            portraitImage.sprite = Resources.Load<Sprite>(line.portrait);
+        }
+        else
+        {
+            nextButton.SetActive(false);  // Hide the Next button
+            returnButton.SetActive(true); // Show the Return button when dialogue ends
+        }
+    }
+
+    public void AdvanceDialogue()
+    {
+        currentLine++;
+        ShowLine();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); // Make sure the scene is added in Build Settings
+    }
+}
