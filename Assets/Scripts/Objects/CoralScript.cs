a35bfd78b976a5f5cb8bd115f7afe210e276c949
MegaSonicWarhead <51359439+MegaSonicWarhead@users.noreply.github.com>
2025-04-06 23:35:43 +0200

    Coral add

---
+++ Assets/Scripts/Objects/CoralScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralScript : MonoBehaviour
+{
+    // Reference to the player's OxygenBar
+    public OxygenBar oxygenBar;
+
+    // When the player enters the coral's trigger zone
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))  // Check if the colliding object is the player
+        {
+            // Set the player's oxygen to maximum
+            if (oxygenBar != null)
+            {
+                oxygenBar.RefillOxygen(100f);  // Refills the oxygen to maximum (100)
+                Debug.Log("Oxygen refilled to 100!");
+            }
+            else
+            {
+                Debug.LogWarning("OxygenBar reference is not assigned in the Inspector!");
+            }
+        }
+    }
+}
