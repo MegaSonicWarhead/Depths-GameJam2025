ecd5e415d9031c8e08238ed4dc3d643f439fc66e
Benzo00o <itzthewildboi@gmail.com>
2025-04-07 19:36:37 +0200

    money semi working
    money visually increases on UI but does not decrease

--- Assets/Scripts/PlayerMoney.cs
+++ Assets/Scripts/PlayerMoney.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMoney : MonoBehaviour
 {
     private int money = 0;
 
+    public TextMeshProUGUI coinText;
+
     // Method to add money
     public void AddMoney(int amount)
     {
         money += amount;
+        money = Mathf.Max(0, money);
+        UpdateUI();
     }
 
     // Method to get the current amount of money
     public int GetMoney()
     {
         return money;
+    }
+
+    private void UpdateUI()
+    {
+        if (coinText != null)
+        {
+            coinText.text = "$: " + money;
+        }
+        
     }
 }
