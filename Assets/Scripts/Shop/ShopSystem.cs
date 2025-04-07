28991423a7ce3eee6fe83a820a7877573bcd2f96
Benzo00o <itzthewildboi@gmail.com>
2025-04-07 11:56:39 +0200

    getting shop working

---
+++ Assets/Scripts/Shop/ShopSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSystem : MonoBehaviour
+{
+    public GameObject shopUI;
+    private bool playerInRange = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleShopUI();
+        }
+    }
+    private void ToggleShopUI()
+    {
+        bool isActive = shopUI.activeSelf;
+        shopUI.SetActive(!isActive);
+
+        if (!isActive)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            shopUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
