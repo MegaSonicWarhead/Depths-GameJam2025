0300df45d29e8d6a738440c6c6322ad5df535822
Palesa2323 <palesagaetsewe52@gmail.com>
2025-04-07 18:33:49 +0200

    Update CoralScript.cs

--- Assets/Scripts/Objects/CoralScript.cs
+++ Assets/Scripts/Objects/CoralScript.cs
@@ -6,6 +6,7 @@ public class CoralScript : MonoBehaviour
 {
     // Reference to the player's OxygenBar
     public OxygenBar oxygenBar;
+    public AudioManager audioManager; 
 
     // When the player enters the coral's trigger zone
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,6 +22,15 @@ public class CoralScript : MonoBehaviour
             else
             {
                 Debug.LogWarning("OxygenBar reference is not assigned in the Inspector!");
+            }
+
+            if (audioManager != null)
+            {
+                audioManager.PlayBubble();  // Play the bubble sound effect
+            }
+            else
+            {
+                Debug.LogWarning("AudioManager is not assigned in the Inspector!");
             }
         }
     }
